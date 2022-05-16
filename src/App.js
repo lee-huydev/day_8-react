3ada4039cc3a41249d1a1620c69c9610bc44db32
Huy <lee@Huy.local>
2022-05-16 17:26:54 +0700

    note thing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { RotatingLines } from 'react-loader-spinner';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const api = 'https://jsonplaceholder.typicode.com/todos';
+   const [isLoading, setLoading] = useState(false);
+   const [todos, setTodos] = useState([]);
+   const [scroll, setScroll] = useState(false)
+   const handleScroll = (e) => {
+      let scrollValue = e.target.documentElement.scrollTop
+      console.log(scrollValue);
+      scrollValue > 20 ? setScroll(true) : setScroll(false)
+      
+   };
+   useEffect(() => {
+      window.addEventListener('scroll', handleScroll);
+      return () => window.removeEventListener('scroll', handleScroll);
+   });
+   const handleOnclick = () => {
+      setLoading(true);
+      setTimeout(async () => {
+         const res = await fetch(api);
+         const data = await res.json();
+         setTodos(data);
+         setLoading(false);
+      }, 3000);
+   };
+   console.log(todos);
+   return (
+      <>
+         {todos && todos.map((todo) => <h1 key={todo.id}>{todo.title}</h1>)}
+         {scroll ? <button style={{position:'fixed' ,top:'50%', right:'20px', padding:'20px'}}>Up</button> : null}
+         <button onClick={handleOnclick}>
+            {isLoading ? (
+               <span>
+                  {' '}
+                  <RotatingLines width="30px" />{' '}
+               </span>
+            ) : (
+               'Fetch Data'
+            )}
+         </button>
+      </>
+   );
 }
 
 export default App;
